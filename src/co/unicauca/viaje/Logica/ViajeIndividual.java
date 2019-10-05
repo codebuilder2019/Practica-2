3fece91077b375f7f9b5cc7e449045582509a40b
codebuilder2019 <gjaime@unicauca.edu.co>
2019-10-04 19:09:40 -0500

    Practica 2

---
+++ src/co/unicauca/viaje/Logica/ViajeIndividual.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.unicauca.viaje.Logica;
+
+import co.unicauca.viaje.Logica.Viaje;
+import java.util.Date;
+
+/**
+ *
+ * @author AdrianFelipe
+ */
+public class ViajeIndividual extends Viaje {
+
+    public ViajeIndividual(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada) {
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+    }
+
+    
+ @Override
+ public String descripcion() {
+ return "Disfruta tu viaje individual";
+ }
+ //No se sobreescribe cualquierMetodo2()
+}
